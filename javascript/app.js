5926e94391231ebbe054e1b36044880271850087
joydeep-git <joydeep.accounts@tuta.io>
2023-02-13 10:44:42 +0530

    a

--- javascript/app.js
+++ javascript/app.js
@@ -21,10 +21,10 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     getResult()
 }))
 
-function generateComputerChoice(){
-    const randomNumber = Math.trunc(Math.random()* 3)
+function generateComputerChoice() {
+    const randomNumber = Math.trunc(Math.random() * 3)
 
-    if (randomNumber === 0){
+    if (randomNumber === 0) {
         computerChoice = 'rock'
     }
     if (randomNumber === 1) {
@@ -37,32 +37,32 @@ function generateComputerChoice(){
 }
 
 function getResult() {
-    if (computerChoice === userChoice){
+    if (computerChoice === userChoice) {
         result = 'It is Draw!'
     }
-    if (computerChoice === 'rock' && userChoice === 'paper'){
+    if (computerChoice === 'rock' && userChoice === 'paper') {
         result = 'you Won!'
-        userScore = userScore +1
+        userScore = userScore + 1
     }
-    if (computerChoice === 'rock' && userChoice === 'scissor'){
+    if (computerChoice === 'rock' && userChoice === 'scissor') {
         result = 'you Lost!'
-        computerScore = computerScore +1;
+        computerScore = computerScore + 1;
     }
-    if (computerChoice === 'paper' && userChoice === 'scissor'){
+    if (computerChoice === 'paper' && userChoice === 'scissor') {
         result = 'you Won!'
-        userScore = userScore +1
+        userScore = userScore + 1
     }
-    if (computerChoice === 'paper' && userChoice === 'rock'){
+    if (computerChoice === 'paper' && userChoice === 'rock') {
         result = 'you Lost!'
-        computerScore = computerScore +1;
+        computerScore = computerScore + 1;
     }
-    if (computerChoice === 'scissor' && userChoice === 'rock'){
+    if (computerChoice === 'scissor' && userChoice === 'rock') {
         result = 'you Won!'
-        userScore = userScore +1
+        userScore = userScore + 1
     }
-    if (computerChoice === 'scissor' && userChoice === 'paper'){
+    if (computerChoice === 'scissor' && userChoice === 'paper') {
         result = 'you Lost!'
-        computerScore = computerScore +1;
+        computerScore = computerScore + 1;
     }
     resultDisplay.innerHTML = result;
     score.innerHTML = userScore;
